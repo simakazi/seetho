00cd53699ca702139e11334dfa39c73b02875589
Semenoff.Andrew <Semenoff.Andrew@152e8884-9bd4-11de-8050-81a99a42e534>
2009-09-08 19:06:28 +0000

    adding feed-filters into pulls and filtering works

--- reader/forms/feedform.py
+++ reader/forms/feedform.py
@@ -2,5 +2,11 @@
 from django import forms
 
 class FeedForm(forms.Form):
-    url = forms.URLField(label="Resource URL",initial="http://",max_length=300)
+    feed_url = forms.URLField(label="Resource URL",initial="http://",max_length=300)
+    feed_cheked = forms.BooleanField(initial=False)
+    filter_must=forms.CharField(initial="",max_length=300,required=False)
+    filter_may=forms.CharField(initial="",max_length=300,required=False)
+    filter_not=forms.CharField(initial="",max_length=300,required=False)
+    pull_title=forms.CharField(initial="New pull",max_length=100)
+    pull_id=forms.IntegerField()
     
