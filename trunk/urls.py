fa686948dccf7cc62bdda3962fd5a3488f1be8fe
Semenoff.Andrew <Semenoff.Andrew@152e8884-9bd4-11de-8050-81a99a42e534>
2009-09-08 12:03:54 +0000

    OpenId auth, some stupid css, deeper model

--- trunk/urls.py
+++ trunk/urls.py
@@ -2,15 +2,15 @@
 from django.conf.urls.defaults import *
 import os.path
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 PROJECT_ROOT = os.path.dirname(__file__)
 
 urlpatterns = patterns('',
- (r'^$','reader.views.list_feeds'),
+ (r'^$','reader.views.index'),
  (r'^add_feed/$','reader.views.add_feed'),
  (r'^openid/', include('django_openid_auth.urls')),
- (r'^logout/$', 'django.contrib.auth.views.logout'),
+ (r'^logout/$', 'reader.views.logout'),
  (r'^.*img/(?P<path>.*)$', 'django.views.static.serve',{'document_root': os.path.join(PROJECT_ROOT,'media/img')}),
  (r'^.*css/(?P<path>.*)$', 'django.views.static.serve',{'document_root': os.path.join(PROJECT_ROOT,'media/css')}),
  (r'^.*js/(?P<path>.*)$', 'django.views.static.serve',{'document_root': os.path.join(PROJECT_ROOT,'media/js')}),
@@ -23,5 +23,5 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/(.*)', admin.site.root),
+     (r'^admin/(.*)', admin.site.root),
 )
